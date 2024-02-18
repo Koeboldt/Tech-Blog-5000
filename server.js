43d53c7288f7e08929e4f2983cb50b61a77c0648
Koeboldt <142840132+Koeboldt@users.noreply.github.com>
2024-02-18 12:38:45 -0600

    added the connection for render, hopefully

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
 
@@ -31,5 +31,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(port, () => console.log('Now listening'));
 });
